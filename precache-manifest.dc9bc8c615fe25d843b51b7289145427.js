c5cab2925eec4b748fbb2a43667da7feee2bd4f6
helmax-y <max_rad@ukr.net>
2019-12-12 23:37:27 +0200

    Updates

---
+++ precache-manifest.dc9bc8c615fe25d843b51b7289145427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c9f08516cdf69d78ba454d046dee47",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "4efefbe5846a368c426e",
+    "url": "/react_todo-app/static/css/main.94ff5a9f.chunk.css"
+  },
+  {
+    "revision": "5a76bad6fc1e05f761a2",
+    "url": "/react_todo-app/static/js/2.583b85e5.chunk.js"
+  },
+  {
+    "revision": "4efefbe5846a368c426e",
+    "url": "/react_todo-app/static/js/main.4c21524c.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
